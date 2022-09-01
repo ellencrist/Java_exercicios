804c3c8ad0190db0ec483d207f72d4692c45b872
Ellen Cristine <86074197+ellencrist@users.noreply.github.com>
2022-09-01 14:58:23 -0300

    Update compradetvs.java

--- op_IF_ELSE/subtracao_e_if_CompradeTv's/compradetvs.java
+++ op_IF_ELSE/subtracao_e_if_CompradeTv's/compradetvs.java
@@ -1,9 +1,11 @@
 public static void main(String[] args) {
          
-          //"42"  r$900
-          //"50" r$905 a 1115,00
-          //"75" r$acima de R$1250
-          
+         //Tenho um orçamento de 1250 reais para comprar uma TV, o objetivo aqui é calcular o troco que terei de acordo com a minha escolha de compra
+          //TV de 42 polegadas - custo: R$900
+          //TV de 50 polegadas - custo: de R$905 a R$1115,00
+          //TV de 75 polegadas - Custo: acima de R$1250
+         
+         //var
           int orc;
           orc = 1250;
           
@@ -14,6 +16,8 @@ public static void main(String[] args) {
           
           int resto;
           resto = 0;
+         
+         //lógica
           if(opcao1>resto){
                     System.out.println("Se eu compra a TV de 42 polegadas terei um troco de: " + opcao1 + " reais");     
           }
@@ -24,6 +28,7 @@ public static void main(String[] args) {
           
           if(opcao3>=resto){
                 System.out.println("Se a escolha for a de 75 polegadas poderei ter um troco de " + opcao3 + " reais" );
+         //Fim do Método principal
           }
              
     }
