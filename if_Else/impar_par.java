4fe9ce25f1b7beedc86c2d43c7c42fcbbced363c
Ellen Cristine <86074197+ellencrist@users.noreply.github.com>
2022-09-01 14:28:50 -0300

    Add files via upload

---
+++ if_Else/impar_par.java
@@ -0,0 +1,25 @@
+public static void main(String[] args){
+        //declaraçoes de variaveis
+        int num;
+        num=2;
+
+        int num1;
+        num1=9;
+
+        ///par
+        if(num%2==0){
+        System.out.println("o número é par");
+
+        }else{
+        System.out.println("o número é impar");
+        }
+
+        ///impar
+        if(num1%2==0){
+        System.out.println("o número é par");
+
+        }else{
+        System.out.println("o número é impar");
+        }
+
+}
