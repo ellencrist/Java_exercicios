f17db0ffb24d9e395ec37695c2b5f674fcc0cd54
Ellen Cristine <86074197+ellencrist@users.noreply.github.com>
2022-09-10 11:17:48 -0300

    Update lista2.java

--- operacoes_matematica_basica/ao_cubo_ao_quadrado_raizes_math/lista2.java
+++ operacoes_matematica_basica/ao_cubo_ao_quadrado_raizes_math/lista2.java
@@ -1,10 +1,12 @@
 /******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+faça um programa que receba um número positivo e maior que zero,calcule e mostre
+o número digitado ao quadrado;
+o número digitado ao cubo;
+a raí quadrada
+a raíz cúbica
 
+ferramenta utilizada: GDB online debugger
 *******************************************************************************/
 import java.util.*;
 import java.lang.Math;
